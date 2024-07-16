3c68e4c391d1889bc0fb7821c8a3af03eb575aaf
sauravpandey538 <sauravpandey0325@icloud.com>
2024-07-16 21:13:30 +0545

    id type defined

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -2,10 +2,14 @@ import { NextRequest } from "next/server";
 import jwt from 'jsonwebtoken';
 import { writer } from "repl";
 
+interface Payload {
+    id: string | number;
+}
+
 export const getDataFromToken = (request:NextRequest)=>{
 try {
     const token = request.cookies.get('token')?.value || '';
-    const decodedToken = jwt.verify(token,process.env.TOKEN_SECRET_PASSWORD!)
+    const decodedToken:Payload = jwt.verify(token,process.env.TOKEN_SECRET_PASSWORD!)  as Payload
     return decodedToken.id;
 } catch (error : any) {
     throw new Error (error.message)
